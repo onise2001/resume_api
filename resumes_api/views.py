0cc8968933fb0ad7f4b1b88a14f2da5050b137a0
onise2001 <onise.babulashvili@yahoo.com>
2024-06-25 23:02:24 +0400

    Added resume update functionality

--- resumes_api/views.py
+++ resumes_api/views.py
@@ -9,12 +9,13 @@ from rest_framework import status
 
 
 class ResumeViewSet(GenericViewSet):
-    queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
 
 
     def list(self, request):
-        serializer = self.serializer_class(self.queryset, many=True)
+        queryset = Resume.objects.all()
+
+        serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
     def create(self,request):
@@ -40,6 +41,19 @@ class ResumeViewSet(GenericViewSet):
             instance.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def update(self, request, pk=None):
+        instance = self.get_object()
+        if instance:
+            serializer = self.serializer_class(instance=instance, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=status.HTTP_404_NOT_FOUND)
+     
     
     
     
